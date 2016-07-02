a84428a79e2834e47028126ae2cd58ca413859ab
Furt <furt1337@live.com>
2016-07-01 21:47:06 -0500

    More work to hearthstone item

--- src/main/java/me/furt/dexutils/items/ItemHearthstone.java
+++ src/main/java/me/furt/dexutils/items/ItemHearthstone.java
@@ -12,6 +12,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
@@ -21,23 +22,41 @@ public class ItemHearthstone extends Item {
 		this.setCreativeTab(DexUtils.coreTab);
 		this.setMaxStackSize(1);
 	}
-	
+
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand)
-    {
-		if(hand.equals(EnumHand.MAIN_HAND)) {
+	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn,
+			World worldIn, EntityPlayer playerIn, EnumHand hand) {
+		// TODO this should only happen once and not get spammed
+		if (hand.equals(EnumHand.MAIN_HAND)) {
 			Home h = new Home(playerIn, "home");
-			Location l = h.getHome();
-			if(l != null)
-				playerIn.setPositionAndUpdate(l.getX(), l.getY(), l.getZ());
-			
+			if (playerIn.isSneaking()) {
+				h.setHome();
+				playerIn.addChatMessage(new TextComponentString(
+						TextFormatting.GOLD + "Home location is set."));
+			} else {
+				Location l = h.getHome();
+				if (l != null) {
+					playerIn.setPositionAndUpdate(l.getX(), l.getY(), l.getZ());
+					playerIn.addChatMessage(new TextComponentString(
+							TextFormatting.GOLD + "Teleporting to home location."));
+				} else {
+					playerIn.addChatMessage(new TextComponentString(
+							TextFormatting.GOLD + "Home location is not set."));
+					playerIn.addChatMessage(new TextComponentString(
+							TextFormatting.GOLD
+									+ "To set home hold shift and right click."));
+				}
+			}
+
 			h.destroy();
+			return new ActionResult(EnumActionResult.SUCCESS, itemStackIn);
 		}
-        return new ActionResult(EnumActionResult.PASS, itemStackIn);
-    }
-	
+		return new ActionResult(EnumActionResult.PASS, itemStackIn);
+	}
+
 	@Override
-	public void addInformation(ItemStack is, EntityPlayer player, List list, boolean bool) {
+	public void addInformation(ItemStack is, EntityPlayer player, List list,
+			boolean bool) {
 		list.add("Right click to goto home location if set.");
 		list.add(TextFormatting.GOLD + "Hearth is where you make it!");
 	}
