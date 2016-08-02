5479fe21fec862a2cc7e84079ab4c47f789c134a
Furt <furt1337@live.com>
2016-08-02 06:17:27 -0500

    new item worldstone

--- src/main/java/me/furt/dexutils/init/ModItems.java
+++ src/main/java/me/furt/dexutils/init/ModItems.java
@@ -4,6 +4,7 @@ import me.furt.dexutils.help.RegisterHelper;
 import me.furt.dexutils.items.ItemCitrineGem;
 import me.furt.dexutils.items.ItemCoin;
 import me.furt.dexutils.items.ItemHearthstone;
+import me.furt.dexutils.items.ItemWorldstone;
 import net.minecraft.item.Item;
 
 /**
@@ -12,14 +13,20 @@ import net.minecraft.item.Item;
 public class ModItems {
 	public static Item hearthstone = new ItemHearthstone()
 			.setUnlocalizedName("hearthstone");
+	public static Item worldstone = new ItemWorldstone()
+			.setUnlocalizedName("worldstone");
 	public static Item citrine_gem = new ItemCitrineGem()
 			.setUnlocalizedName("citrine_gem");
-	public static Item coinGold = new ItemCoin().setUnlocalizedName("coin_gold");
-	public static Item coinSilver = new ItemCoin().setUnlocalizedName("coin_silver");
-	public static Item coinCopper = new ItemCoin().setUnlocalizedName("coin_copper");
+	public static Item coinGold = new ItemCoin()
+			.setUnlocalizedName("coin_gold");
+	public static Item coinSilver = new ItemCoin()
+			.setUnlocalizedName("coin_silver");
+	public static Item coinCopper = new ItemCoin()
+			.setUnlocalizedName("coin_copper");
 
 	public static void registerItems() {
 		RegisterHelper.registerItem(hearthstone);
+		RegisterHelper.registerItem(worldstone);
 		// RegisterHelper.registerItem(citrine_gem);
 		RegisterHelper.registerItem(coinGold);
 		RegisterHelper.registerItem(coinSilver);
@@ -28,6 +35,7 @@ public class ModItems {
 
 	public static void registerItemRenderer() {
 		RegisterHelper.registerItemRenderer(hearthstone);
+		RegisterHelper.registerItemRenderer(worldstone);
 		// RegisterHelper.registerItemRenderer(citrine_gem);
 		RegisterHelper.registerItemRenderer(coinGold);
 		RegisterHelper.registerItemRenderer(coinSilver);
--- src/main/java/me/furt/dexutils/items/itemWorldstone.java
+++ src/main/java/me/furt/dexutils/items/itemWorldstone.java
@@ -1,5 +1,15 @@
 package me.furt.dexutils.items;
 
-public class itemWorldstone {
+import me.furt.dexutils.DexUtils;
+import net.minecraft.item.Item;
 
+/**
+ * Created by Furt on 7/16/2016.
+ */
+public class ItemWorldstone extends Item {
+	public ItemWorldstone() {
+		super();
+		this.setCreativeTab(DexUtils.coreTab);
+		this.setMaxStackSize(1);
+	}
 }
